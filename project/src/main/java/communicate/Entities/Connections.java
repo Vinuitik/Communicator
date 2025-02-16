65b8034ad2cd22dc12856cbad603c6d07f07192d
Victor <sizonenkodima6@gmail.com>
2025-02-16 22:19:32 +0000

    2025-02-16

--- project/src/main/java/communicate/Entities/Connections.java
+++ project/src/main/java/communicate/Entities/Connections.java
@@ -1,26 +1,16 @@
 package communicate.Entities;
 
-import java.time.LocalDate;
+import jakarta.persistence.*;
+
+import java.util.Objects;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
-
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
+import lombok.*;
 
 @Entity
 @Data
-@Builder
-@AllArgsConstructor
 @NoArgsConstructor
+@AllArgsConstructor
 public class Connections {
 
     @Id
@@ -29,16 +19,28 @@ public class Connections {
     
     private String description;
 
-
     @ManyToOne
-    @JoinColumn(name = "friend_id1")
+    @JoinColumn(name = "friend_id1", nullable = false)
     @JsonBackReference
-    @ToString.Exclude
     private Friend friend1;
 
     @ManyToOne
-    @JoinColumn(name = "friend_id1")
+    @JoinColumn(name = "friend_id2", nullable = false)
     @JsonBackReference
-    @ToString.Exclude
     private Friend friend2;
+
+    // Ensure uniqueness so (A, B) is the same as (B, A)
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Connections)) return false;
+        Connections that = (Connections) o;
+        return (friend1.equals(that.friend1) && friend2.equals(that.friend2)) ||
+               (friend1.equals(that.friend2) && friend2.equals(that.friend1));
+    }
+
+    public int hashCode() {
+        return friend1.getId() < friend2.getId() ?
+                Objects.hash(friend1.getId(), friend2.getId()) :
+                Objects.hash(friend2.getId(), friend1.getId());
+    }
 }
--- project/src/main/java/communicate/Entities/Relationships.java
+++
@@ -1,5 +0,0 @@
-package communicate.Entities;
-
-public class Relationships {
-
-}
