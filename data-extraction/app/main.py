6016ce9d2b7fafbacab22b8351980ddf467c878b
Syzonenko Dmytro <sizonenkodima6@gmail.com>
2025-09-23 13:32:42 +0100

    data extractor service initialized

---
+++ data-extraction/app/main.py
@@ -0,0 +1,13 @@
+
+from llamaindex import TelegramReader, WhatsAppReader,DiscordReader
+
+# Entry point for the data extraction microservice
+
+def main():
+    print("Data Extraction Microservice started.")
+
+if __name__ == "__main__":
+    main()
+
+
+def extractTelegramData():
