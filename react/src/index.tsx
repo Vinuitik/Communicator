1c6d3b1abb4b3aab1c429c212cd20c1430eee83c
Vinuitik <sizonenkodima6@gmail.com>
2026-08-17 15:38:20 +0100

    Wire outbox auto-flush into app boot

--- react/src/index.tsx
+++ react/src/index.tsx
@@ -4,8 +4,10 @@ import App from './App';
 import './styles/globals.css';
 import './pwa/installPrompt';
 import { registerServiceWorker } from './pwa/registerSW';
+import { wireAutoFlush } from './pwa/outbox';
 
 registerServiceWorker();
+wireAutoFlush();
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
